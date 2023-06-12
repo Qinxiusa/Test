0fdf03c5317efc06590c4f1f9d0081fd0fd799a2
cs.chen <cs.chen@tpv-tech.com>
2023-06-12 14:14:23 +0800

    first upload

---
+++ Unit_Form.h
@@ -0,0 +1,31 @@
+﻿//---------------------------------------------------------------------------
+#ifndef Unit_FormH
+#define Unit_FormH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <Buttons.hpp>
+//github上传测试 20230608
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+	TButton *Button2;
+	TBitBtn *BitBtn1;
+	TButton *Button3;
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button3Click(TObject *Sender);
+	void __fastcall BitBtn1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
